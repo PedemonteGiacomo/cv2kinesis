cfa3d771107d5b918c549da6bc312bdd5d83a485
PedemonteGiacomo <giacomopedemonte@libero.it>
2025-07-31 18:50:27 +0200

    Legacy: solo hybrid_pipeline

--- new_image_processing_pipeline/src/medical_image_processing/processing/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Processing algorithms for RSNA pipeline."""
-
-from .threshold_ccl import ThresholdCCL
-from .liver_cc_simple import LiverCCSimple
-
-__all__ = [
-    "ThresholdCCL",
-    "LiverCCSimple",
-]
